60ea65b06ad489786796217ab378132a80cc7d8f
seonmin8284 <seonmin8284@naver.com>
2023-08-24 04:49:23 +0900

    modified:   tools/infer.py 	modified:   .DS_Store 	modified:   README.md 	modified:   main.ipynb 	modified:   main3.ipynb 	deleted:    sample_input/img.png 	deleted:    sample_input/result.png 	modified:   template/logo.png 	deleted:    template/logo2.png

--- tools/infer.py
+++ tools/infer.py
@@ -1,2 +1,37 @@
-tessdata_dir_config = '--tessdata-dir "src"'
-pytesseract.image_to_string(front,lang='kor',config=tessdata_dir_config)
+#0) 라이브러리 선언
+import pytesseract
+
+# tessdata_dir_config = '--tessdata-dir "src"'
+# pytesseract.image_to_string(front,lang='kor',config=tessdata_dir_config)
+
+# 1) pdf이면 이렇게 하기, 아니면 바로 진행
+import pdf2image
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+def pdf_to_img(pdf_file):
+    return pdf2image.convert_from_path(pdf_file)
+
+
+def ocr_core(file):
+    text = pytesseract.image_to_string(file)
+    return text
+
+
+def print_pages(pdf_file):
+    images = pdf_to_img(pdf_file)
+    for pg, img in enumerate(images):
+        print(ocr_core(img))
+
+
+print_pages('sample.pdf')
+
+
+import pandas as pd
+
+frame = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(frame)
